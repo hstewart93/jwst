4a050bdf05ccf475813e7d76cbbe2e9314077636
hstewart93 <harriet.stt@gmail.com>
2025-01-08 17:05:29 +0000

    fix threading issue and hotfix for corrupt fits file issue.

--- simulations/generate_simulated_data.py
+++ simulations/generate_simulated_data.py
@@ -1,13 +1,15 @@
 import os
 import numpy as np
 
-from simulations.soss_simulations import run_simulations_random
+from simulations.soss_simulations import run_simulations
 
-output_directory = "/data/typhon2/hattie/jwst/soss_simulations/"
-number_samples = 10
+output_directory = "/data/typhon2/hattie/jwst/soss_simulations"
+number_samples = 500
 
-simulated_spectra = run_simulations_random(
-    number_samples,
-    os.path.join(output_directory, f"{number_samples}_soss_sims_randomised_target.h5"),
-    N_contaminants="random",
-    )
+run_simulations(
+    N_simulations=number_samples,
+    targ_Jmag=(0, 10),
+    targ_Teff=(2000, 7000),
+    N_contaminants=(0, 10),
+    output_file=os.path.join(output_directory, f"{number_samples}_soss_sims_randomised_target.h5"),
+)
